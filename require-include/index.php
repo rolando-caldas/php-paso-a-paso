3044e39aab4f81e24152e56a1ac2121c71b54e16
rolando-caldas <rolando.caldas@gmail.com>
2013-06-22 20:08:00 +0200

    Uso de include y require - PHP paso a paso

--- require-include/index.php
+++ require-include/index.php
@@ -7,3 +7,23 @@ require_once './include.php';
 require_once './include.php';
 
 echo 'Finalizando ejecuci&oacute;n';
+
+/*
+
+Uso alternativo no recomendado
+------------------------------
+
+include('fichero.php');
+require('fichero.php');
+include_once('fichero.php');
+require_once('fichero.php');
+
+Uso recomendado por estándar
+----------------------------
+
+include 'fichero.php';
+require 'fichero.php';
+include_once 'fichero.php';
+require_once 'fichero.php';
+
+*/
